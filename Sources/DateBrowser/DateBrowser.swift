39123cd9d74859f95097798b0a6ed76b4871bf46
Daniel Pressner <daniel.pressner@gmail.com>
2022-12-01 22:10:18 +0100

    initial commit

--- Sources/DateBrowser/DateBrowser.swift
+++ Sources/DateBrowser/DateBrowser.swift
@@ -1,6 +1,59 @@
-public struct DateBrowser {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import SwiftUI
+
+public struct DateBrowser: View {
+  
+  @Binding var selectedDate: Date
+  
+  public var body: some View {
+    HStack {
+      Button {
+        
+      } label: {
+        Image(systemName: "chevron.backward.to.line")
+      }
+      Button {
+        
+      } label: {
+        Image(systemName: "chevron.left")
+      }
+      Text(selectedDate.formatted(date: .abbreviated, time: .omitted))
+      Button {
+        
+      } label: {
+        Image(systemName: "chevron.right")
+      }
+      Button {
+        
+      } label: {
+        Image(systemName: "chevron.forward.to.line")
+      }
+      Button {
+        
+      } label: {
+        Image(systemName: "calendar")
+      }
+      Button {
+        
+      } label: {
+        Image(systemName: "calendar.day.timeline.left")
+      }
     }
+  }
+  
+  public init(selectedDateBinding: Binding<Date>) {
+    _selectedDate = selectedDateBinding
+  }
 }
+
+extension DateBrowser {
+  func adjustDate(by magnitude: Int, timeUnit: Int) {}
+}
+
+struct DateBrowser_Previews: PreviewProvider {
+  
+  static var previews: some View {
+    DateBrowser(selectedDateBinding: .constant(Date()))
+  }
+}
+
